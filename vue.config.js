6f9eed01260374f02665a30a7851f1f61e7ef707
yanzhao <1770285990@qq.com>
2022-05-07 17:24:24 +0800

    [新增功能](master): proxy

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,44 @@ console.log('========================', process.env.NODE_ENV)
 
 
 module.exports = {
-    lintOnSave:false
+    lintOnSave: false,
+    publicPath: process.env.NODE_ENV === "production" ? "https://prdo.com" : "http://localhost:8040",
+    outputDir: 'dist',
+    assetsDir: '',
+    devServer: {
+        port: 8040,
+        compress: true,
+        // proxy: "http://localhost:8070"
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8030',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': '/'        // 重定向到:http://192.168.1.16:8085/xxxx
+                }
+            },
+            'prod': {
+                target: 'http:127.0.0.1:8030',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/prod': "/prod"  // 重定向到:http://192.168.1.16:8085/prod/xxxx
+                }
+
+            }
+
+        }
+
+    }
+
+    // 基础路径
+    // publicPath:"/",
+    // devServer:{
+    //     // 路由匹配路径
+    //     '/api':{
+    //         target:"http://localhost:3000",
+    //         changeOrigin:true,
+    //     }
+    //
+    // }
 
 }
